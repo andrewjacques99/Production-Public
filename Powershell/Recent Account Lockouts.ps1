0b7dd873f50b2f34840c14ca33f91fc288b8f9c0
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-01-10 14:44:20 +0000

    Added -ErrorAction SilentlyContinue
    Removed the Red error message, when nothing is found.
    Downside this does hide all Error actions.

--- Powershell/Recent Account Lockouts.ps1
+++ Powershell/Recent Account Lockouts.ps1
@@ -3,6 +3,6 @@ $DCList = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
 foreach ($DC in $DCList.DomainControllers)
     {
     $DC.Name
-    Get-WinEvent -ComputerName $DC.Name -LogName Security -FilterXPath "*[System[EventID=4740 and TimeCreated[timediff(@SystemTime) <= 36000000]] ]" |
+    Get-WinEvent -ComputerName $DC.Name -LogName Security -FilterXPath "*[System[EventID=4740 and TimeCreated[timediff(@SystemTime) <= 36000000]] ]" -ErrorAction SilentlyContinue |
     Select-Object @{Name='Lockout Time';Expression={$_.TimeCreated}},@{Name='User Name';Expression={$_.Properties[0].Value}},@{Name='Source Host';Expression={$_.Properties[1].Value}} | ft -AutoSize
    Set-ExecutionPolicy Unrestricted }
