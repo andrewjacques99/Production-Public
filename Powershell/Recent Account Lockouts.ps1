023382ccbe0f816280a70a0a9f81f51ed4432e02
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-01-11 10:53:48 +0000

    Update comments

--- Powershell/Recent Account Lockouts.ps1
+++ Powershell/Recent Account Lockouts.ps1
@@ -6,6 +6,9 @@ foreach ($DC in $DCList.DomainControllers)
     Get-WinEvent -ComputerName $DC.Name -LogName Security -FilterXPath "*[System[EventID=4740 and TimeCreated[timediff(@SystemTime) <= 36000000]] ]" -ErrorVariable +errs -ErrorAction SilentlyContinue |
     Select-Object @{Name='Lockout Time';Expression={$_.TimeCreated}},@{Name='User Name';Expression={$_.Properties[0].Value}},@{Name='Source Host';Expression={$_.Properties[1].Value}} | ft -AutoSize
    Set-ExecutionPolicy Unrestricted }
-<#Non-Terminating Errors have been silenced and will not display
+<#
+.Description
+Non-Terminating Errors have been silenced and will not display
 Non-Terminating Errors output have been added to a appending variable +errs
-To display the Non-Terminating Errors type $errs after the script has run.#>
+To display the Non-Terminating Errors type $errs after the script has run.
+#>
