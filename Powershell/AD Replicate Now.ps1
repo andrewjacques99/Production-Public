e88106bc8fd9febfec07c0370a8eff63a9cd7738
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-11 11:50:51 +0000

    Create AD Replicate Now.ps1

---
+++ Powershell/AD Replicate Now.ps1
@@ -0,0 +1,8 @@
+﻿$DC = Get-ADDomainController -Discover -Service PrimaryDC
+
+repadmin /syncall $DC /AedP | Out-Null
+repadmin /syncall $DC /Aed  | Out-Null
+
+repadmin /showrepl| ConvertFrom-Csv 
+"`n"
+repadmin /queue $DC
