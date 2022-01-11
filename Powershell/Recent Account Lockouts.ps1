6c21461c5dfe3fe3e256b7efec7f25c60e730c08
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-01-11 09:51:40 +0000

    Added -ErrorVariable +errs
    Add the -Error Variable and +errs, so if this needs to be looked at the variable can be called.

--- Powershell/Recent Account Lockouts.ps1
+++ Powershell/Recent Account Lockouts.ps1
@@ -3,6 +3,6 @@ $DCList = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
 foreach ($DC in $DCList.DomainControllers)
     {
     $DC.Name
-    Get-WinEvent -ComputerName $DC.Name -LogName Security -FilterXPath "*[System[EventID=4740 and TimeCreated[timediff(@SystemTime) <= 36000000]] ]" -ErrorAction SilentlyContinue |
+    Get-WinEvent -ComputerName $DC.Name -LogName Security -FilterXPath "*[System[EventID=4740 and TimeCreated[timediff(@SystemTime) <= 36000000]] ]" -ErrorVariable +errs -ErrorAction SilentlyContinue |
     Select-Object @{Name='Lockout Time';Expression={$_.TimeCreated}},@{Name='User Name';Expression={$_.Properties[0].Value}},@{Name='Source Host';Expression={$_.Properties[1].Value}} | ft -AutoSize
    Set-ExecutionPolicy Unrestricted }
