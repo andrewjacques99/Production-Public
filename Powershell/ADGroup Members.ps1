217f4ebda39ee6dcb1f99f282731affcbfc99d48
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-12 11:58:23 +0000

    Create ADGroup Members.ps1

---
+++ Powershell/ADGroup Members.ps1
@@ -0,0 +1,17 @@
+﻿$OUList = Get-ADGroup -Filter * -Properties Name,DistinguishedName | Select-Object -Property Name,DistinguishedName
+
+$OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
+$ADGroupFilter = "*"
+
+
+$ADGroups = $OU
+
+foreach ($ADGroup in $ADGroups)
+    {
+    Write-Host $ADGroup.Name
+    Write-Host "------------"
+    $ADGroupMember = Get-ADGroupMember -Identity $ADGroup.DistinguishedName
+    $ADGroupMember.Name
+    # Write-Host $ADGroupMember.name -Separator ","
+    Write-Host ""
+    }
