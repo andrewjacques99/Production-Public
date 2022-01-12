1bbf9c516a14bbdb9b1c9664cd55a64a6aa98ba6
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-12 11:08:32 +0000

    Create AD OU Members.ps1

---
+++ Powershell/AD OU Members.ps1
@@ -0,0 +1,12 @@
+﻿$OUList = Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName | Select-Object -Property Name,DistinguishedName
+
+$OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
+$ADUserFilter = "*"
+
+
+$ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter $ADUserFilter
+
+foreach ($ADUser in $ADUsers)
+    {
+    Write-Host $ADUser.Name
+    }
