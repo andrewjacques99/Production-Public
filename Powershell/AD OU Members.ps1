ef921a17aac20d2cd163fa36a4f060741d50e822
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-01-13 10:24:55 +0000

    Changed how the data is displayed
    Changed the code so, that the OU is displayed in the list to look like a header/title of the list .
    Added the Name, Enabled and Last Logon Date

--- Powershell/AD OU Members.ps1
+++ Powershell/AD OU Members.ps1
@@ -3,11 +3,9 @@ $OUList = Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName 
 $OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
 $ADUserFilter = "*"
 
+$ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter $ADUserFilter -Properties LastLogOnDate | Select-Object -Property Name,Enabled,LastLogOnDate
 
-$ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter $ADUserFilter
-
-foreach ($ADUser in $ADUsers)
-    {
-    Write-Host $ADUser.Name
-    }
+$OU.DistinguishedName
+Write-Host "------------"
+$ADUsers
 Clear-Variable ADuser
