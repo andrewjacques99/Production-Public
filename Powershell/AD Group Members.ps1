0210a4179333585c3ac96a63f1f1fd4472965096
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-17 09:09:12 +0000

    Update AD Group Members.ps1
    New features: more AD Information about the members: Account Status, and Last Log On Time. Added more clear Variables. Additional comments

--- Powershell/AD Group Members.ps1
+++ Powershell/AD Group Members.ps1
@@ -1,19 +1,24 @@
+#Gets a list of Groups
 $OUList = Get-ADGroup -Filter * -Properties Name,DistinguishedName | Select-Object -Property Name,DistinguishedName
 
+#Create a Gridview list of Groups, to uses as a selection
 $OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
-$ADGroupFilter = "*"
 
+#Gets the AD Group members SamAccoountName as per the Group selected, converts the Output to a String
+$ADGroupMember = @(Get-ADGroupMember -identity $OU.Name | Select-Object -Property SamAccountName | select -ExpandProperty SamAccountName -Unique | Out-String -stream)
 
-$ADGroups = $OU
+#Check each SamAccountName againest the Active Driectory and display the Name, Account Status and Last Log on Date
+ForEach ($ADUser in $ADGroupMember)
+{
+$ADListUsers = Get-ADUser $ADUser -Properties LastLogOnDate | Select-Object -Property Name,Enabled,LastLogOnDate
+$ADListUsers
+}
 
-foreach ($ADGroup in $ADGroups)
-    {
-    Write-Host $ADGroup.Name
-    Write-Host "------------"
-    $ADGroupMember = Get-ADGroupMember -Identity $ADGroup.DistinguishedName
-    $ADGroupMember.Name
-    # Write-Host $ADGroupMember.name -Separator ","
-    Write-Host ""
-    }
-    Clear-Variable ADGroup
-    Clear-Variable OUList
+#If "Cannot find an object with identity: SamAccountName", this is probally another Group, check your AD to confirm
+
+#Clears the Variables, this stops any issue with the variables bring back any previous held information
+Clear-Variable ADGroups
+Clear-Variable OUList
+Clear-Variable OU
+Clear-Variable ADUser
+Clear-Variable ADGroupMember
