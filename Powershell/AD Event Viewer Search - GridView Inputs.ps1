d16dcbcbc7cd78521d0c25241dfdb95ac1be4175
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-19 07:49:39 +0000

    New

---
+++ Powershell/AD Event Viewer Search - GridView Inputs.ps1
@@ -0,0 +1,19 @@
+﻿# Gets a list of Computers
+$ComputerList = Get-ADComputer -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
+$ComputerSelect = $ComputerList | Out-GridView -Title "Select Computer Name and Click OK" -OutputMode Single
+
+
+# Gets a list of Event Log Names
+$LogNameList = Get-EventLog -LogName * -ComputerName $ComputerSelect.Name | Sort-Object | Select-Object -Property Log
+$LogSelect = $LogNameList | Out-GridView -Title "Select Log Name and Click OK" -OutputMode Single
+
+# Gets a list of the Event ID's
+$EventNameList = Get-WinEvent -LogName $LogSelect.Log -ComputerName $ComputerSelect.Name | Sort-Object | Select-Object -Property ProviderName,ID,Message
+$EventNameSelect = $EventNameList | Out-GridView -Title "Select Event Number and Click OK" -OutputMode Single
+
+# Combineds the above and outputs
+$GetLogEvent = Get-WinEvent -LogName Application -ComputerName ATGRDDK01 | Where-Object ID -eq $EventNameSelect.Id
+$GetLogEvent
+
+# Clears all the variables
+Remove-Variable * -ErrorAction SilentlyContinue
---
+++ Powershell/AD Event Viewer Search - Manual Inputs.ps1
@@ -0,0 +1,44 @@
+﻿# Starting Message
+Write-Output "Computer Name and Log Name are required inputs"
+
+
+# Computer name input
+$ComputerFilter = Read-Host -Prompt "Computer Name (Example: adxadk01)"
+
+
+# Last x Minutes
+
+$Minutes = 12000
+
+# Start Search
+$FormatEnumerationLimit = -1
+$Seconds = $Minutes*60*1000
+$Output=@()
+$Computers = Get-ADComputer -Filter * | where {($_.Name -like $ComputerFilter) -and ($_.Name -notlike "*xx")}
+
+# Log Name Input
+$LogName = Read-Host -Prompt "Log Name (Example: Application)"
+
+# Event ID Input
+$EventID = Read-Host -Prompt "Event ID (Example: 3012)"
+
+# Message Filter Input
+$MessageFilter = Read-Host -Prompt "Message Filter (Example: * Wild Card or *Citrix*)"
+
+# Different FilterPath depending on Input 
+If ($EventID -eq "")
+{$FilterPath = "*"}
+Else
+{$FilterPath = "*[System[EventID=" + $EventID + " and TimeCreated[timediff(@SystemTime) <= $Seconds]] ]"}
+
+# Combined information to get data
+foreach ($Computer in $Computers)
+    {
+    # Write-Host $Computer.name
+    $Output += Get-WinEvent -ComputerName $Computer.Name -LogName $LogName -FilterXPath $FilterPath | Where {$_.Message -like $MessageFilter} | Select-Object TimeCreated, MachineName, ID, Message, UserID
+    }
+    $Output | Sort-Object TimeCreated | Out-GridView
+
+# Clear all variables
+Remove-Variable * -ErrorAction SilentlyContinue
+
