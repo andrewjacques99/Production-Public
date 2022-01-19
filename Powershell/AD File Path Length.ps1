0ad67031212269ce77d2aa73a4cc19ef1fc24d45
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-19 11:29:07 +0000

    New

---
+++ Powershell/AD File Path Length.ps1
@@ -0,0 +1,51 @@
+﻿# Gets a list of Computers
+$ComputerList = Get-ADComputer -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
+$ComputerSelect = $ComputerList | Out-GridView -Title "Select Computer Name and Click OK" -OutputMode Single
+
+
+
+$MinimumPathLengthsToShow = Read-Host -Prompt ‘Set this to 260 to find problematic paths in Windows. (Default is 0)’
+
+if ([string]::IsNullOrWhiteSpace($MinimumPathLengthsToShow))
+
+{
+
+$MinimumPathLengthsToShow = ‘0’
+
+}
+
+$WriteResultsToConsole = $true
+
+$filePathsAndLengths = [System.Collections.ArrayList]::new()
+
+$DirectoryPathToScan = Read-Host -Prompt "File Path"
+
+
+$ListFolderItems = Invoke-Command -ComputerName $ComputerSelect.Name -ScriptBlock {
+
+Get-ChildItem -Path $args[0] -Recurse -Force |
+Select-Object -Property FullName, @{Name = "FullNameLength"; Expression = { ($_.FullName.Length) } } |
+Sort-Object -Property FullNameLength -Descending 
+
+} -ArgumentList $DirectoryPathToScan
+
+$ComputerSelect.Name
+Write-Output "----------"
+
+# Show the list of folder/files per Minimum Path Lengths  
+$ListFolderItems | ForEach-Object {
+
+$filePath = $_.FullName
+$length = $_.FullNameLength
+
+# If this path is long enough, add it to the results.
+if ($length -ge $MinimumPathLengthsToShow)
+    {
+        [string] $lineOutput = "$length : $filePath"
+
+        if ($WriteResultsToConsole) { Write-Output $lineOutput }
+
+        $filePathsAndLengths.Add($_) > $null
+    }
+}
+    
