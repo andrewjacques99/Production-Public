1807cf0d79abac7417f9e9d517aa3e1693565271
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-20 14:11:36 +0000

    Update AD Event Viewer Search - Manual Inputs.ps1
    Removed the date restriction field, add in additional user messages.

--- Powershell/AD Event Viewer Search - Manual Inputs.ps1
+++ Powershell/AD Event Viewer Search - Manual Inputs.ps1
@@ -1,35 +1,41 @@
-# Starting Message
-Write-Output "Computer Name and Log Name are required inputs"
-
-
+﻿
 # Computer name input
-$ComputerFilter = Read-Host -Prompt "Computer Name (Example: adxadk01)"
+Write-Host "-Mandatory-" -ForegroundColor Red
+$ComputerFilter =  Read-Host -Prompt "Computer Name (Example: adxadk01)"
 
-
-# Last x Minutes
-
-$Minutes = 12000
-
-# Start Search
-$FormatEnumerationLimit = -1
-$Seconds = $Minutes*60*1000
 $Output=@()
+
+# Computer Name Check
 $Computers = Get-ADComputer -Filter * | where {($_.Name -like $ComputerFilter) -and ($_.Name -notlike "*xx")}
 
 # Log Name Input
+Write-Host "-Mandatory-" -ForegroundColor Red
 $LogName = Read-Host -Prompt "Log Name (Example: Application)"
 
 # Event ID Input
+Write-Host "-If Required-" -ForegroundColor Green
 $EventID = Read-Host -Prompt "Event ID (Example: 3012)"
 
 # Message Filter Input
-$MessageFilter = Read-Host -Prompt "Message Filter (Example: * Wild Card or *Citrix*)"
+Write-Host "-If Required-" -ForegroundColor Green
+$MessageFilter = Read-Host -Prompt "Message Filter (Example: *Citrix*)"
+
+# Message Filter Defaul Input if nothing enter
+If ([string]::IsNullOrWhiteSpace($MessageFilter))
+
+{
+
+$MessageFilter = ‘*’
+
+}
+
 
 # Different FilterPath depending on Input 
 If ($EventID -eq "")
 {$FilterPath = "*"}
 Else
-{$FilterPath = "*[System[EventID=" + $EventID + " and TimeCreated[timediff(@SystemTime) <= $Seconds]] ]"}
+{$FilterPath = "*[System[EventID=" + $EventID + " ] ]"}
+
 
 # Combined information to get data
 foreach ($Computer in $Computers)
@@ -41,4 +47,3 @@ foreach ($Computer in $Computers)
 
 # Clear all variables
 Remove-Variable * -ErrorAction SilentlyContinue
-
