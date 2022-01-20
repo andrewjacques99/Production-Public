9d3b0cb176bd7061aae02303e42cfb5f9da7b34e
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-20 10:40:56 +0000

    Update AD Event Viewer Search - GridView Inputs.ps1
    Updated

--- Powershell/AD Event Viewer Search - GridView Inputs.ps1
+++ Powershell/AD Event Viewer Search - GridView Inputs.ps1
@@ -1,18 +1,20 @@
-# Gets a list of Computers
+﻿# Gets a list of Computers
 $ComputerList = Get-ADComputer -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
 $ComputerSelect = $ComputerList | Out-GridView -Title "Select Computer Name and Click OK" -OutputMode Single
-
+Write-Host "Computer Name: " $ComputerSelect.Name
 
 # Gets a list of Event Log Names
 $LogNameList = Get-EventLog -LogName * -ComputerName $ComputerSelect.Name | Sort-Object | Select-Object -Property Log
 $LogSelect = $LogNameList | Out-GridView -Title "Select Log Name and Click OK" -OutputMode Single
+Write-Host "Log Name: " $LogSelect.Log
 
 # Gets a list of the Event ID's
 $EventNameList = Get-WinEvent -LogName $LogSelect.Log -ComputerName $ComputerSelect.Name | Sort-Object | Select-Object -Property ProviderName,ID,Message
 $EventNameSelect = $EventNameList | Out-GridView -Title "Select Event Number and Click OK" -OutputMode Single
+Write-Host "Event : " $EventNameSelect.Id
 
-# Combineds the above and outputs
-$GetLogEvent = Get-WinEvent -LogName Application -ComputerName ATGRDDK01 | Where-Object ID -eq $EventNameSelect.Id
+# Combinds the above and outputs
+$GetLogEvent = Get-WinEvent -LogName Application -ComputerName $ComputerSelect.Name | Where-Object ID -eq $EventNameSelect.Id
 $GetLogEvent
 
 # Clears all the variables
