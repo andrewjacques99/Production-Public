61468e265c7ae8d5a8ec322dd1b595bee469ccf2
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-21 14:07:14 +0000

    Update AD Group Members.ps1
    Update variable names to group instead of UV and Output Group Name.

--- Powershell/AD Group Members.ps1
+++ Powershell/AD Group Members.ps1
@@ -1,11 +1,14 @@
-#Creates a list of Groups
-$OUList = Get-ADGroup -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
+#Gets a list of Groups
+$GroupList = Get-ADGroup -Filter * -Properties Name,DistinguishedName | Sort-Object  | Select-Object -Property Name,DistinguishedName
 
 #Create a Gridview list of Groups, to uses as a selection
-$OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
+$Group = $GroupList | Out-GridView -Title "Select Group and Click OK" -OutputMode Single
 
 #Gets the AD Group members SamAccoountName as per the Group selected, converts the Output to a String
-$ADGroupMember = @(Get-ADGroupMember -identity $OU.Name | Select-Object -Property SamAccountName | select -ExpandProperty SamAccountName -Unique | Out-String -stream)
+$ADGroupMember = @(Get-ADGroupMember -identity $Group.Name | Select-Object -Property SamAccountName | select -ExpandProperty SamAccountName -Unique | Out-String -stream)
+
+$Group.DistinguishedName
+Write-Host "------------"
 
 #Check each SamAccountName againest the Active Driectory and display the Name, Account Status and Last Log on Date
 ForEach ($ADUser in $ADGroupMember)
