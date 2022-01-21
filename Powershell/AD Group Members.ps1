ae534b0a781b3b962aca1af076607b79a4c92df3
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-01-21 12:48:56 +0000

    Update AD Group Members.ps1
    Changed the Grid-View Title from OU to Group

--- Powershell/AD Group Members.ps1
+++ Powershell/AD Group Members.ps1
@@ -2,7 +2,7 @@
 $OUList = Get-ADGroup -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
 
 #Create a Gridview list of Groups, to uses as a selection
-$OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
+$OU = $OUList | Out-GridView -Title "Select Group and Click OK" -OutputMode Single
 
 #Gets the AD Group members SamAccoountName as per the Group selected, converts the Output to a String
 $ADGroupMember = @(Get-ADGroupMember -identity $OU.Name | Select-Object -Property SamAccountName | select -ExpandProperty SamAccountName -Unique | Out-String -stream)
