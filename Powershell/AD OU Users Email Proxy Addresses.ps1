163dfa005ec2115f3206e7fdce5c29661ff025e2
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-27 15:07:08 +0000

    Create AD OU Users Email Proxy Addresses.ps1
    New

---
+++ Powershell/AD OU Users Email Proxy Addresses.ps1
@@ -0,0 +1,27 @@
+﻿#Gets a list of OU.
+$OUList = Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
+
+#Create a Gridview list of Groups, to uses as a selection.
+$OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
+
+#Gets the AD Users Email Proxy Address as per the OU selected.
+$ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter * -Properties proxyAddresses | ForEach {
+    ForEach ($Address in $_.proxyAddresses) {
+        [pscustomobject]@{
+            User = $_.Name
+            ProxyAddress = $Address
+        }
+    }
+}
+
+
+#Display the OU Path.
+$OU.DistinguishedName
+Write-Host "------------"
+
+#Displays the AD User Name and Proxy Address. 
+$ADUsers
+
+#Clears the Variables, this stops any issue with the variables bring back any previous held information.
+Remove-Variable * -ErrorAction SilentlyContinue
+
