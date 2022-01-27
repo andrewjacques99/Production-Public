2b1f8b244f06d200fe1228bfd4ad6ab934fb1c76
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-27 14:26:29 +0000

    New

--- Powershell/AD Group Members Filter For Enabled or Disabled.ps1
+++ Powershell/AD Group Members Filter For Enabled or Disabled.ps1
@@ -7,13 +7,28 @@ $Group = $GroupList | Out-GridView -Title "Select Group and Click OK" -OutputMod
 #Gets the AD Group members SamAccoountName as per the Group selected, converts the Output to a String
 $ADGroupMember = @(Get-ADGroupMember -identity $Group.Name | Select-Object -Property SamAccountName | select -ExpandProperty SamAccountName -Unique | Out-String -stream)
 
+$FilterPick = Read-Host "Type Enabled or Disabled (Default is Enabled)"
+If ([string]::IsNullOrWhiteSpace($FilterPick))
+{
+$FilterOption = $True
+}
+ElseIf ($FilterPick -eq 'Enabled')
+{
+$FilterOption = $True
+}
+Else
+{
+$FilterOption = $False
+}
+
+
 $Group.DistinguishedName
 Write-Host "------------"
 
 #Check each SamAccountName againest the Active Driectory and display the Name, Account Status and Last Log on Date
 ForEach ($ADUser in $ADGroupMember)
 {
-$ADListUsers = Get-ADUser $ADUser -Properties LastLogOnDate | Where {$_.Enabled -eq $true} | Select-Object -Property Name,Enabled,LastLogOnDate
+$ADListUsers = Get-ADUser $ADUser -Properties LastLogOnDate | Where {$_.Enabled -eq $FilterOption} | Select-Object -Property Name,Enabled,LastLogOnDate
 $ADListUsers
 }
 
--- Powershell/AD OU Members Filter For Enabled or Disabled.ps1
+++ Powershell/AD OU Members Filter For Enabled or Disabled.ps1
@@ -4,8 +4,22 @@ $OUList = Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName 
 #Create a Gridview list of Groups, to uses as a selection
 $OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
 
+$FilterPick = Read-Host "Type Enabled or Disabled (Default is Enabled)"
+If ([string]::IsNullOrWhiteSpace($FilterPick))
+{
+$FilterOption = $True
+}
+ElseIf ($FilterPick -eq 'Enabled')
+{
+$FilterOption = $True
+}
+Else
+{
+$FilterOption = $False
+}
+
 #Gets the AD Users as per the OU selected, Added the LastLogOnDate properties, display the Name. Enablesand LastLogOnDate
-$ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter * -Properties LastLogOnDate | Where {$_.Enabled -eq $true} | Select-Object -Property Name,Enabled,LastLogOnDate
+$ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter * -Properties LastLogOnDate | Where {$_.Enabled -eq $FilterOption} | Select-Object -Property Name,Enabled,LastLogOnDate
 
 #Display the OU Path
 $OU.DistinguishedName
