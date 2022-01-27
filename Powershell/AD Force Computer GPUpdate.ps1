efc3ffa7a3d56e72fefa0283eb361a91e47fc35f
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-27 11:17:18 +0000

    Create AD Force Computer GPUpdate.ps1
    New

---
+++ Powershell/AD Force Computer GPUpdate.ps1
@@ -0,0 +1,10 @@
+﻿# Gets a list of Computers
+$ComputerList = Get-ADComputer -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
+$ComputerSelect = $ComputerList | Out-GridView -Title "Select Computer Name and Click OK" -OutputMode Single
+Write-Host "Computer: " $ComputerSelect.Name
+
+foreach ($Server in $ComputerSelect)
+    {
+    Invoke-Command -ComputerName $Server.Name -ScriptBlock {Gpupdate /Force}
+    Invoke-Command –ScriptBlock {GPResult /r /SCOPE COMPUTER} –ComputerName $Server.Name
+    }
