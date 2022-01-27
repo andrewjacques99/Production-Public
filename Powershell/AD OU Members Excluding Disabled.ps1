8f4a420c601842e8b2ff3d20e714a2d283cd1145
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-27 11:56:14 +0000

    New
    New

---
+++ Powershell/AD OU Members Excluding Disabled.ps1
@@ -0,0 +1,16 @@
+﻿#Gets a list of OU
+$OUList = Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
+
+#Create a Gridview list of Groups, to uses as a selection
+$OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
+
+#Gets the AD Users as per the OU selected, Added the LastLogOnDate properties, display the Name. Enablesand LastLogOnDate
+$ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter * -Properties LastLogOnDate | Where {$_.Enabled -eq $true} | Select-Object -Property Name,Enabled,LastLogOnDate
+
+#Display the OU Path
+$OU.DistinguishedName
+Write-Host "------------"
+$ADUsers
+
+#Clears the Variables, this stops any issue with the variables bring back any previous held information
+Remove-Variable * -ErrorAction SilentlyContinue
