21ca1b5eeb90b759a253e96c61867ac5804656cf
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-01-31 09:33:33 +0000

    Create AD Server OS list.ps1
    New

---
+++ Powershell/AD Server OS list.ps1
@@ -0,0 +1,6 @@
+﻿# Gets a list of Computers
+$ComputerList = Get-ADComputer -Filter * -Properties  Name,Operatingsystem, OperatingSystemVersion, OperatingSystemServicePack,IPv4Address | Sort-Object -Property Operatingsystem | Select-Object -Property Name,Operatingsystem, OperatingSystemVersion, OperatingSystemServicePack, IPv4Address
+$ComputerSelect = $ComputerList | Out-GridView -Title "Sorted By Operation System"
+
+#Clears the Variables, this stops any issue with the variables bring back any previous held information
+Remove-Variable * -ErrorAction SilentlyContinue
