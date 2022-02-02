9b9749bd4cfe94636a020c6b4c5d22fee1e936d1
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-02-02 12:23:44 +0000

    New
    New

---
+++ Powershell/AD OU Set All To Not Change Password On Next Logon.ps1
@@ -0,0 +1,17 @@
+﻿#Gets a list of OU
+$OUList = Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
+
+#Create a Gridview list of Groups, to uses as a selection
+$OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
+
+$ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter * | Where {$_.Enabled -eq 'True'}
+
+
+foreach ($ADUser in $ADUsers)
+    {
+    Write-Host $ADUser.Name
+    Set-ADUser -Identity $ADUser -ChangePasswordAtLogon:$False
+    }
+
+#Clears the Variables, this stops any issue with the variables bring back any previous held information
+Remove-Variable * -ErrorAction SilentlyContinue
