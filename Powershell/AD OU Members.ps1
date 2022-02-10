f9d8938ef5b37192710df4d07b66f62447f31ac8
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-02-10 09:53:31 +0000

    Update AD OU Members.ps1
    Added space to comments

--- Powershell/AD OU Members.ps1
+++ Powershell/AD OU Members.ps1
@@ -1,16 +1,16 @@
-#Gets a list of OU
+# Gets a list of OU
 $OUList = Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName | Sort-Object  | Select-Object -Property Name,DistinguishedName
 
-#Create a Gridview list of Groups, to uses as a selection
+# Create a Gridview list of Groups, to uses as a selection
 $OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
 
-#Gets the AD Users as per the OU selected, Added the LastLogOnDate properties, display the Name. Enablesand LastLogOnDate
+# Gets the AD Users as per the OU selected, Added the LastLogOnDate properties, display the Name. Enablesand LastLogOnDate
 $ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter * -Properties LastLogOnDate | Select-Object -Property Name,Enabled,LastLogOnDate
 
-#Display the OU Path
+# Display the OU Path
 $OU.DistinguishedName
 Write-Host "------------"
 $ADUsers
 
-#Clears the Variables, this stops any issue with the variables bring back any previous held information
+# Clears the Variables, this stops any issue with the variables bring back any previous held information
 Remove-Variable * -ErrorAction SilentlyContinue
