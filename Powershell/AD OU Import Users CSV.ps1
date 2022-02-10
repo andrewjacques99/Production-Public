1829a16181638e730a381b3960e58bc204c49a40
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-02-10 10:04:25 +0000

    Update AD OU Import Users CSV.ps1
    Add space to comments

--- Powershell/AD OU Import Users CSV.ps1
+++ Powershell/AD OU Import Users CSV.ps1
@@ -30,7 +30,7 @@ $ADUsers = Import-csv $CVLocation
 # Loop through each row containing user details in the CSV file 
 foreach ($User in $ADUsers)
 {
-	#Read user data from each field in each row and assign the data to a variable as below
+	# Read user data from each field in each row and assign the data to a variable as below
 		
 	$Username 	= $User.username
 	$Password 	= $User.password
@@ -48,17 +48,17 @@ foreach ($User in $ADUsers)
     
 
 
-	#Check to see if the user already exists in AD
+	# Check to see if the user already exists in AD
 	if (Get-ADUser -F {SamAccountName -eq $Username})
 	{
-		 #If user does exist, give a warning
+		 # If user does exist, give a warning
 		 Write-Warning "A user account with username $Username already exist in Active Directory."
 	}
 	else
 	{
-		#User does not exist then proceed to create the new user account
+		# User does not exist then proceed to create the new user account
 		
-        #Account will be created in the OU provided by the $OU variable read from the CSV file
+        # Account will be created in the OU provided by the $OU variable read from the CSV file
 		New-ADUser `
             -SamAccountName $Username `
             -Name "$Firstname $Lastname" `
