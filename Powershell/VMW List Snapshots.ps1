2b21fb676f26fb3f63b7741e6ef72c99e4bf5ca0
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-02-10 11:16:03 +0000

    Update VMW List Snapshots.ps1
    Updated comments

--- Powershell/VMW List Snapshots.ps1
+++ Powershell/VMW List Snapshots.ps1
@@ -1,17 +1,18 @@
+# Get PSSesion
 Get-PSSession | Remove-PSSession
 
+# Imports the Server names
 $VSphereServerImport = Import-Csv -Path "C:\Powershell Scripts\CSV\VMServers.csv"
 
 # Import Modue vmware viautomation core 
 Import-Module vmware.vimautomation.core
 
+# Connect to teh VMWare server and pulls back the snapsots into a grid-view
 Connect-VIServer $VSphereServerImport.Server
 $SnapShotList = Get-VM | Get-SnapShot | Select Created, VM, Name, Description, SizeGB
 $SnapShotCount = $SnapShotList | Measure
 Write-Host "Snapshot Count = " $SnapShotCount.count
-#$SnapShotListOutput = $VSphereServerImport, $SnapShotList
 $SnapShotList | Sort-Object -Property VM | Out-GridView -Title "VMWare Snapshot List"
-
 
 # Clears the Variables, this stops any issue with the variables bring back any previous held information
 Remove-Variable * -ErrorAction SilentlyContinue
