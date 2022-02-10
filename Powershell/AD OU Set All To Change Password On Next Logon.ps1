74d50c7e25f60fc22b5c246d2290706497cc2795
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-02-10 09:54:20 +0000

    Update AD OU Set All To Change Password On Next Logon.ps1
    Added space to comments

--- Powershell/AD OU Set All To Change Password On Next Logon.ps1
+++ Powershell/AD OU Set All To Change Password On Next Logon.ps1
@@ -1,7 +1,7 @@
-#Gets a list of OU
+# Gets a list of OU
 $OUList = Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
 
-#Create a Gridview list of Groups, to uses as a selection
+# Create a Gridview list of Groups, to uses as a selection
 $OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
 
 $ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter * | Where {$_.Enabled -eq 'True'}
@@ -13,5 +13,5 @@ foreach ($ADUser in $ADUsers)
     Set-ADUser -Identity $ADUser -ChangePasswordAtLogon:$True
     }
 
-#Clears the Variables, this stops any issue with the variables bring back any previous held information
+# Clears the Variables, this stops any issue with the variables bring back any previous held information
 Remove-Variable * -ErrorAction SilentlyContinue
