1d76f31cf062e1b7c4d0ebbedb007c79e6cbba10
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-02-10 09:47:50 +0000

    Update AD Group Members.ps1
    Added space to Comments

--- Powershell/AD Group Members.ps1
+++ Powershell/AD Group Members.ps1
@@ -1,23 +1,23 @@
-#Gets a list of Groups
+# Gets a list of Groups
 $GroupList = Get-ADGroup -Filter * -Properties Name,DistinguishedName | Sort-Object  | Select-Object -Property Name,DistinguishedName
 
-#Create a Gridview list of Groups, to uses as a selection
+# Create a Gridview list of Groups, to uses as a selection
 $Group = $GroupList | Out-GridView -Title "Select Group and Click OK" -OutputMode Single
 
-#Gets the AD Group members SamAccoountName as per the Group selected, converts the Output to a String
+# Gets the AD Group members SamAccoountName as per the Group selected, converts the Output to a String
 $ADGroupMember = @(Get-ADGroupMember -identity $Group.Name | Select-Object -Property SamAccountName | select -ExpandProperty SamAccountName -Unique | Out-String -stream)
 
 $Group.DistinguishedName
 Write-Host "------------"
 
-#Check each SamAccountName againest the Active Driectory and display the Name, Account Status and Last Log on Date
+# Check each SamAccountName againest the Active Driectory and display the Name, Account Status and Last Log on Date
 ForEach ($ADUser in $ADGroupMember)
 {
 $ADListUsers = Get-ADUser $ADUser -Properties LastLogOnDate | Select-Object -Property Name,Enabled,LastLogOnDate
 $ADListUsers
 }
 
-#If "Cannot find an object with identity: SamAccountName", this is probally another Group, check your AD to confirm
+# If "Cannot find an object with identity: SamAccountName", this is probally another Group, check your AD to confirm
 
-#Clears the Variables, this stops any issue with the variables bring back any previous held information
+# Clears the Variables, this stops any issue with the variables bring back any previous held information
 Remove-Variable * -ErrorAction SilentlyContinue
