716a1c09f550bc16d63e7f0a53116c5547b8d700
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-02-10 09:55:53 +0000

    Update AD Server OS list.ps1
    Added space to comments

--- Powershell/AD Server OS list.ps1
+++ Powershell/AD Server OS list.ps1
@@ -2,5 +2,5 @@
 $ComputerList = Get-ADComputer -Filter * -Properties  Name,Operatingsystem, OperatingSystemVersion, OperatingSystemServicePack,IPv4Address | Sort-Object -Property Operatingsystem | Select-Object -Property Name,Operatingsystem, OperatingSystemVersion, OperatingSystemServicePack, IPv4Address
 $ComputerSelect = $ComputerList | Out-GridView -Title "Sorted By Operation System"
 
-#Clears the Variables, this stops any issue with the variables bring back any previous held information
+# Clears the Variables, this stops any issue with the variables bring back any previous held information
 Remove-Variable * -ErrorAction SilentlyContinue
